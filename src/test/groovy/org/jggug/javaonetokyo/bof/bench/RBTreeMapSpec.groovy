c72c8daaaaaf1d32be350e53c07361cab9b0c687
Yasuharu NAKANO <ynak@jggug.org>
2012-03-19 10:43:25 +0900

    [from now] 2012/03/19 10:43:25
     .../javaonetokyo/bof/bench/RBTreeMapSpec.groovy    |   57 ++++++++++++++++++++
     1 file changed, 57 insertions(+)

--- src/test/groovy/org/jggug/javaonetokyo/bof/bench/RBTreeMapSpec.groovy
+++ src/test/groovy/org/jggug/javaonetokyo/bof/bench/RBTreeMapSpec.groovy
@@ -149,4 +149,61 @@ class RdBTreeMapSpec extends Specification {
         map.root.right.right.left in EmptyNode
         map.root.right.right.left.parent == map.root.right.right
     }
+
+    def "エントリを5つ追加する"() {
+        when:
+        map.put('a', 'Value of a')
+        map.put('c', 'Value of c')
+        map.put('X', 'Value of X')
+        map.put('b', 'Value of b')
+        map.put('d', 'Value of d')
+
+        then:
+        map.height() == 3
+
+        map.root in FillNode
+        map.root.parent == null
+        map.root.color == Color.BLACK
+        map.root.entry.key == 'a'
+        map.root.entry.value == 'Value of a'
+
+        map.root.left in FillNode
+        map.root.left.parent == map.root
+        map.root.left.color == Color.BLACK
+        map.root.left.entry.key == 'X'
+        map.root.left.entry.value == 'Value of X'
+
+        map.root.left.right in EmptyNode
+        map.root.left.right.parent == map.root.left
+        map.root.left.left in EmptyNode
+        map.root.left.left.parent == map.root.left
+
+        map.root.right in FillNode
+        map.root.right.parent == map.root
+        map.root.right.color == Color.BLACK
+        map.root.right.entry.key == 'c'
+        map.root.right.entry.value == 'Value of c'
+
+        map.root.right.left in FillNode
+        map.root.right.left.parent == map.root.right
+        map.root.right.left.color == Color.RED
+        map.root.right.left.entry.key == 'b'
+        map.root.right.left.entry.value == 'Value of b'
+
+        map.root.right.left.right in EmptyNode
+        map.root.right.left.right.parent == map.root.right.left
+        map.root.right.left.left in EmptyNode
+        map.root.right.left.left.parent == map.root.right.left
+
+        map.root.right.right in FillNode
+        map.root.right.right.parent == map.root.right
+        map.root.right.right.color == Color.RED
+        map.root.right.right.entry.key == 'd'
+        map.root.right.right.entry.value == 'Value of d'
+
+        map.root.right.right.right in EmptyNode
+        map.root.right.right.right.parent == map.root.right.right
+        map.root.right.right.left in EmptyNode
+        map.root.right.right.left.parent == map.root.right.right
+    }
 }
