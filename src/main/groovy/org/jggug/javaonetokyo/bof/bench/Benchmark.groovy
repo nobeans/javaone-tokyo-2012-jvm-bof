de5d751ff50829c6fc911bd6e6b4859672eb3463
Yasuharu NAKANO <ynak@jggug.org>
2012-03-22 05:38:04 +0900

    [from now] 2012/03/22 05:38:04
     .../jggug/javaonetokyo/bof/bench/Benchmark.groovy  |   10 +++++++++-
     1 file changed, 9 insertions(+), 1 deletion(-)

--- src/main/groovy/org/jggug/javaonetokyo/bof/bench/Benchmark.groovy
+++ src/main/groovy/org/jggug/javaonetokyo/bof/bench/Benchmark.groovy
@@ -3,18 +3,26 @@ package org.jggug.javaonetokyo.bof.bench
 import gbench.*
 
 final FILE = new File("src/main/resources/rbtree_map_input.csv")
+final DEBUG = new File("/tmp/rbtree_map_input.csv")
 
-new BenchmarkBuilder().run(idle:0, times:1, average:true, trim:true) {
+new BenchmarkBuilder().run(warmUpTime:0, measureCpuTime:false, verbose:true) {
+
     with "${FILE}を読み込んで赤黒木を構築する", {
         def map = RBTreeMap.newInstance()
+
+        // put(key, value)
         FILE.eachLine { line ->
             def (key, value, height) = line.split(",", 3)
             map.put(key, value)
             //assert map.height() == height as int
+            //DEBUG << "${key},${value},${map.height()}\n"
         }
+
+        // get(key)
         FILE.eachLine { line ->
             def (key, value, height) = line.split(",", 3)
             assert map.get(key) == value
         }
     }
+
 }.prettyPrint()
