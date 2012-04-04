cfee8b9b63ae3c8c0a52378bf11ed101b1ca2871
Yasuharu NAKANO <ynak@jggug.org>
2012-04-04 16:26:04 +0900

    twitter: added comments

--- script/translateAndTweet.groovy
+++ script/translateAndTweet.groovy
@@ -1,17 +1,21 @@
+// Twitter4JのJarを自動ダウンロード＆クラスパス設定する。
 @Grab('org.twitter4j:twitter4j-core:[2.1,)')
 import twitter4j.*
 
 import java.net.URLEncoder
 import groovy.util.XmlParser
 
+// Bing TranslatorのAPIコードを取得する。
 def getApiId = {
     new File(System.properties["user.home"], ".bing-api-id").text.trim()
 }
 
+// URLエンコードする動的メソッドをStringに追加する。
 String.metaClass.encodeAsURL = {
     URLEncoder.encode(delegate)
 }
 
+// Bing Translator APIで翻訳する動的メソッドをStringに追加する。
 String.metaClass.translate = { from, to ->
     def text =  delegate.encodeAsURL()
 
@@ -21,12 +25,17 @@ String.metaClass.translate = { from, to ->
     def response = translateUrl.toURL().text
     return new XmlParser().parseText(response).text()
 
+    // もっとシンプルにも書ける。
     //return new XmlParser().parse(translateUrl).text()
 }
 
-String.metaClass.tweet = { ->
+// Twitterにツイートする。
+String.metaClass.tweet = {
     new TwitterFactory().instance.updateStatus(delegate)
 }
+
+// ------------------------------------------------
+// エレガントに実行する。
 
 System.in.text.translate("ja", "en").tweet()
 
