4555f2602763830a727ed4d3cba709c8a52f0de7
Yasuharu NAKANO <ynak@jggug.org>
2012-04-12 01:49:00 +0900

    added main method to Fibonacci and fobonacci task to build.gradle

--- src/main/groovy/org/jggug/javaonetokyo/bof/bench/Fibonacci.groovy
+++ src/main/groovy/org/jggug/javaonetokyo/bof/bench/Fibonacci.groovy
@@ -6,4 +6,9 @@ class Fibonacci {
              (n == 1) ?  1 :
              call(n - 1) + call(n - 2)
     }
+
+    static void main(String[] args) {
+        def num = args[0] as int
+        println "Fib($num) = ${new Fibonacci().calc(num)}"
+    }
 }
